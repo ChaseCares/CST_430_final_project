0f2f6e02f6399195182bd7d5b638430a4846db4b
Chase C <13863948+ChaseCares@users.noreply.github.com>
2023-04-28 20:47:55 -0400

    add create_profile/iso, add_user improved helpers

--- helpers.sh
+++ helpers.sh
@@ -20,29 +20,61 @@ function echo_yellow_newline() {
     printf "\033[0;33m%s\033[0m\\n" "$1"
 }
 
+function restore_cursor_position() {
+    printf "\033[u"
+}
+function save_cursor_position() {
+    printf "\033[s"
+}
+
+function clean_terminal() {
+    printf "\033[2J"
+    printf "\033[0;0H"
+}
+
 function prompt_user_choice {
+    # clean_terminal
+
     local PS3="$1"
     local message="$2"
-    local options=("${@:3}") # all arguments after
+    local options=("${@:3}")
+
+    echo "$message"
     user_choice=""
 
-    printf "\\n%s\\n" "$message"
-
-    select user_choice in "${options[@]}"; do
+    select user_choice in "${options[@]}" "Manual input" "Cancel"; do
         if [[ -n "$user_choice" ]]; then
             break
         else
             echo_red_newline "Invalid choice"
         fi
     done
+
+    if [ "$user_choice" == "Cancel" ]; then
+        user_choice=""
+        echo "Canceled"
+        return
+    elif [ "$user_choice" == "Manual input" ]; then
+        prompt_user_input "Enter value: "
+    fi
+
 }
 
 function prompt_user_input {
+    # clean_terminal
+
     local message="$1"
+    message+=" or 'c' to cancel: "
     user_input=""
 
     echo -n "$message"
     read -r user_input
+
+    if [ "$user_choice" == "c" ]; then
+        user_choice=""
+        echo "Canceled"
+        return
+    fi
 }
 
 function check_root() {
--- main.sh
+++ main.sh
@@ -6,21 +6,18 @@ source helpers.sh
 mounted_dirs=()
 
 function mount_nfs() {
-    prompt_user_input "Enter local directory: "
+    prompt_user_input "Enter local directory"
     local local_dir="$user_input"
     mounted_dirs+=("$local_dir")
     echo "${mounted_dirs[@]}"
 
-    prompt_user_input "Enter remote directory: "
+    prompt_user_input "Enter remote directory"
     local remote_dir="$user_input"
 
-    prompt_user_choice "Select server: " "Select server to mount from" "${servers[@]}" "Manual input" "Cancel"
+    prompt_user_choice "Select server: " "Select server to mount from" "${servers[@]}"
 
-    if [ "$user_choice" == "Cancel" ]; then
-        echo "Canceled"
-        return
-    elif [ "$user_choice" == "Manual input" ]; then
-        prompt_user_input "Enter ip: "
+    if [ "$user_choice" == "Manual input" ]; then
+        prompt_user_input "Enter ip"
         server_ip="$user_input"
     else
         server="${user_choice}_ip"
@@ -48,7 +45,7 @@ function mount_nfs() {
 }
 
 function unmount_nfs() {
-    prompt_user_choice "Select directory: " "Select directory to unmount" "${mounted_dirs[@]}" "Cancel"
+    prompt_user_choice "Select directory: " "Select directory to unmount" "${mounted_dirs[@]}"
     local dir="$user_choice"
 
     if umount "$dir"; then
@@ -58,20 +55,147 @@ function unmount_nfs() {
     fi
 }
 
-function main() {
-    # check_root
-    while true; do
-        prompt_user_choice "Select action: " "Select action to perform" "Mount" "Unmount" "Cancel"
+function edit_file() {
+    local file="$1"
+    local data="$2"
 
-        if [ "$user_choice" == "Cancel" ]; then
-            echo "Canceled"
-            exit
-        elif [ "$user_choice" == "Mount" ]; then
-            mount_nfs
-        elif [ "$user_choice" == "Unmount" ]; then
-            unmount_nfs
+    if [ ! -f "$file" ]; then
+        if touch "$file"; then
+            echo_green_newline "Created ${file}"
+        else
+            echo_red_newline "Failed to create ${file}"
+            return
         fi
-    done
+    fi
+
+    if grep -q "$data" "$file"; then
+        echo_yellow_newline "Found ${data} in ${file}"
+    else
+        echo_green_newline "Did not find ${data} in ${file}, appending..."
+        echo "$data" >>"$file"
+    fi
 }
 
-main
+function add_user() {
+    local profile_dir="$1"
+
+    prompt_user_input "Enter username"
+    local username="$user_input"
+
+    prompt_user_choice "Select user id: " "Select user id" "Default '${default_user_id}'"
+
+    if [ "$user_choice" == "Default '${default_user_id}'" ]; then
+        user_id="$default_user_id"
+    else
+        user_id="$user_choice"
+    fi
+
+    prompt_user_choice "Select group id: " "Select group id" "Default '${default_group_id}'"
+
+    if [ "$user_choice" == "Default '${default_group_id}'" ]; then
+        group_id="$default_group_id"
+    else
+        group_id="$user_choice"
+    fi
+
+    prompt_user_choice "Select group name: " "Select group name" "Default '${default_group_name}'"
+
+    if [ "$user_choice" == "Default '${default_group_name}'" ]; then
+        local group_name="$default_group_name"
+    else
+        local group_name="$user_choice"
+    fi
+
+    prompt_user_input "User information"
+    local user_input_info="$user_input"
+
+    prompt_user_input "Enter password"
+    local password="$user_input"
+
+    #passwd
+    echo_yellow_newline "Adding user '${username}' to passwd..."
+    edit_file "${profile_dir}/airootfs/etc/passwd" "${username}:x:${user_id}:${group_id}:${user_input_info}:/home/${username}:/usr/bin/bash"
+
+    #shadow
+    echo_yellow_newline "Adding user '${username}' to shadow..."
+    # Generate password hash
+    local pass_hash
+    pass_hash=$(openssl passwd -6 "$password")
+    edit_file "${profile_dir}/airootfs/etc/shadow" "${username}:${pass_hash}:${days_since_Jan_1}:${min_pw_age}:${max_pw_age}:${pw_warning_period}:${pw_inactivity_period}:${account_expi_date}:::"
+
+    #group
+    echo_yellow_newline "Adding user '${username}' to group..."
+    edit_file "${profile_dir}/airootfs/etc/group" "${group_name}:x:${group_id}:${username}"
+
+    #gshadow
+    echo_yellow_newline "Adding user '${username}' to gshadow..."
+    edit_file "${profile_dir}/airootfs/etc/gshadow" "${group_name}:!::${username}"
+}
+
+function create_iso() {
+    prompt_user_choice "Profile: " "Select source profile" "${profiles[@]}"
+    local profile="$user_choice"
+
+    if [[ " ${profiles[*]} " =~ ${profile} ]]; then
+        profile_source="${profiles_dir}/${profile}"
+    else
+        echo_red_newline "No source profile found"
+        return
+    fi
+
+    echo_yellow_newline "Creating iso..."
+
+    if [ ! -d "$iso_out" ]; then
+        if ! mkdir --parents "$iso_out"; then
+            echo_red_newline "Failed to create ${iso_out}"
+            return
+        fi
+    fi
+
+    local iso_dest
+    iso_dest="${iso_out}/ArchLinux-${profile}-$(date +%Y.%m.%d)-x86_64"
+
+    if ! mkarchiso -w "/tmp/archiso-tmp" -o "$iso_dest" "$profile_source"; then
+        echo_red_newline "Failed to create iso"
+        return
+    fi
+
+}
+
+function create_profile() {
+    prompt_user_choice "Profile: " "Select source profile" "${profiles[@]}"
+    local profile="$user_choice"
+
+    if [[ " ${profiles[*]} " =~ ${profile} ]]; then
+        profile_source="${profiles_dir}/${profile}"
+    else
+        echo_red_newline "No source profile found"
+        return
+    fi
+
+    prompt_user_input "Enter profile name"
+    local profile_name="$user_input"
+
+    local profiles_dest="$profiles_out/$profile_name"
+
+    if [ ! -d "$profiles_dest" ]; then
+        if ! mkdir --parents "$profiles_dest"; then
+            echo_red_newline "Failed to create ${profiles_dest}"
+            return
+        fi
+    fi
+
+    if cp --recursive --preserve "$profile_source/." "$profiles_dest/"; then
+        echo_green_newline "Created profile ${profile_name}"
+    else
+        echo_red_newline "Failed to create profile ${profile_name}"
+    fi
+
+    # Update config to add profile to the list
+    if ! grep -q "$profile_name" config; then
+        sed -i "s/profiles=(/profiles=(${profile_name} /" config
+    fi
+
+}
+
+create_iso
